97ea887b3034f43b68024c947a60f53f05da2a39
Tomin Joy <tominjk007@gmail.com>
2022-11-04 04:16:33 +0530

    .

--- bankers_algo.c
+++ bankers_algo.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-int main()
-{
+int main(){
     int n, m, i, j, k, y, alloc[20][20], max[20][20], avail[50], ind = 0;
     printf("Enter the no of Proceses:");
     scanf("%d", &n);
     printf("Enter the no of Resources:");
     scanf("%d", &m);
     printf("Enter the Allocation Matrix:");
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++){
         for (j = 0; j < m; j++)
             scanf("%d", &alloc[i][j]);
     }
     printf("Enter the Max Matrix:");
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++){
         for (j = 0; j < m; j++)
             scanf("%d", &max[i][j]);
     }
@@ -23,43 +20,31 @@ int main()
         scanf("%d", &avail[i]); // Reading the available matrix
     int finish[n], safesequence[n], work[m], need[n][m];
     // calculating NEED matrix
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++){
         for (j = 0; j < m; j++)
             need[i][j] = max[i][j] - alloc[i][j];
     }
     printf("NEED matrix is");
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++){
         printf("\n");
         for (j = 0; j < m; j++)
             printf(" %d ", need[i][j]);
     }
     for (i = 0; i < m; i++)
-    {
         work[i] = avail[i];
-    }
     for (i = 0; i < n; i++)
-    {
         finish[i] = 0;
-    }
-    for (k = 0; k < n; k++)
-    {
-        for (i = 0; i < n; i++)
-        {
-            if (finish[i] == 0)
-            {
+    for (k = 0; k < n; k++){
+        for (i = 0; i < n; i++){
+            if (finish[i] == 0){
                 int flag = 0;
-                for (j = 0; j < m; j++)
-                {
-                    if (need[i][j] > work[j])
-                    {
+                for (j = 0; j < m; j++){
+                    if (need[i][j] > work[j]){
                         flag = 1;
                         break;
                     }
                 }
-                if (flag == 0)
-                {
+                if (flag == 0){
                     safesequence[ind++] = i;
                     for (y = 0; y < m; y++)
                         work[y] += alloc[i][y];
