2f0152aae175ff1cf812d8c63af31d59f43ab6ee
Tomin Joy <tominjk007@gmail.com>
2022-11-08 21:18:05 +0530

    symtab search fixed

--- exp4/pass1.c
+++ exp4/pass1.c
@@ -1,16 +1,38 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+int searchSymtab(char label[]){
+    char symbol[10];
+    FILE *symtab;
+    int value;
+    symtab=fopen("symtab.txt","r");
+    while(!feof(symtab)){
+        fscanf(symtab,"%s %d",symbol,&value);
+		printf("%s %s\n",label,symbol);
+
+        if(!strcmp(label,symbol)){
+			fclose(symtab);
+            return 1;
+        }
+    }
+	fclose(symtab);
+	
+    return 0;
+}
 int searchOpcode(char opcode[]){
 	FILE *optab;
 	char op[10];
 	optab = fopen("optab.txt","r");
 	while(!feof(optab)){
 		fscanf(optab,"%s",op);
+		printf("%s %s\n",opcode,op);
+
 		if(!strcmp(op,opcode)){
+			fclose(optab);
 			return 1;
 		}
 	}
+	fclose(optab);
 	return 0;
 }
 int main(){
@@ -19,7 +41,6 @@ int main(){
 	char label[10],opcode[10],operand[10];
 	in = fopen("input.txt","r");
 	out = fopen("intermediate.txt","w");
-	sym = fopen("symtab.txt","w");
 	if(!feof(in)){
 		fscanf(in,"%s%s%d",label,opcode,&op);
 		if(!strcmp(opcode,"START")){
@@ -44,34 +65,47 @@ int main(){
 				}
 			locctr+=3;
 			
-		}else{
-			if(searchOpcode(opcode)){
-				locctr+=3;
-			}
-			else if(!strcmp(opcode,"WORD")){
-				fprintf(sym,"%s %d\n",label,locctr);
-				locctr+=3;
+		}
+		else{
+			if(searchSymtab(label)){
+				printf("Symbol Error\n");
+			}else{
+				if(searchOpcode(opcode)){
+					locctr+=3;
+				}
+				else if(!strcmp(opcode,"WORD")){
+					sym = fopen("symtab.txt","a");
+					fprintf(sym,"%s %d\n",label,locctr);
+					fclose(sym);
+					locctr+=3;
+					
+				}
+				else if(!strcmp(opcode,"RESW")){
+					sym = fopen("symtab.txt","a");
+					fprintf(sym,"%s %d\n",label,locctr);
+					fclose(sym);
+					op=	atoi(operand);
+					op*=3;
+					locctr+=op;
+					
+				}
+				else if(!strcmp(opcode,"BYTE")){
+					sym = fopen("symtab.txt","a");
+					fprintf(sym,"%s %d\n",label,locctr);
+					fclose(sym);
+					locctr+=(strlen(operand)-3);
+				}
 				
-			}
-			else if(!strcmp(opcode,"RESW")){
-				fprintf(sym,"%s %d\n",label,locctr);
-				op=	atoi(operand);
-				op*=3;
-				locctr+=op;
-				
-			}
-			else if(!strcmp(opcode,"BYTE")){
-				fprintf(sym,"%s %d\n",label,locctr);
-				locctr+=(strlen(operand)-3);
-			}
-			
-			else{
-				printf("opcode error");
+				else{
+					printf("opcode error");
+				}
 			}
 		}
 		
 	}
 	printf("length = %d\n",locctr-sA);
+	fclose(in);
+	fclose(out);
 	return 0;
 			
 }
